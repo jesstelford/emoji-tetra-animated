056b61d95ddbf8ace7b6f54379334422a922e647
Jess Telford <jess+github@jes.st>
2018-04-30 02:36:14 +1000

    Correctly reference the gifsicle binary

--- src/generate-gif.js
+++ src/generate-gif.js
@@ -6,7 +6,7 @@ module.export = () =>
     .shell('gifski -o anim.gif --fps 3 --quality=100 screens/*.png', { stdio: 'inherit' })
     .then((result) => {
       console.log('gifski:', result);
-      return execa.shell('gifsicle -b -O3 --colors 256 anim.gif', { stdio: 'inherit' });
+      return execa.shell(`${gifsicle} -b -O3 --colors 256 anim.gif`, { stdio: 'inherit' });
     })
     .then((result) => {
       console.log('gifsicle:', result);
